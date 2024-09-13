cc9df70bee4cc3cce466be0ebfff4c111f457cc8
BlazingAsher <opensource@davidhui.ca>
2024-09-12 22:15:30 -0400

    document options

--- app/utils/constants.py
+++ app/utils/constants.py
@@ -3,5 +3,5 @@ import os
 LISTEN_ADDRESS = os.getenv("LISTEN_ADDRESS", "127.0.0.1")
 LISTEN_PORT = int(os.getenv("LISTEN_PORT", "8000"))
 
-SCAN_FREQUENCY = int(os.getenv("SCAN_FREQUENCY", "900"))
-SCAN_DURATION = int(os.getenv("SCAN_DURATION", "15"))
+SCAN_FREQUENCY = int(os.getenv("SCAN_FREQUENCY", "900")) # How often we should scan for BLE advertisements (seconds)
+SCAN_DURATION = int(os.getenv("SCAN_DURATION", "15")) # How long each scan should last (seconds)
